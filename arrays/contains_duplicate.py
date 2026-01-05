33f157a947ce7937ec4f8a0648d6cd2b281999d4
Dinesh B <bdinesh170601@gmail.com>
2026-01-05 08:35:02 +0530

    LeetCode 217: Contains Duplicate

---
+++ arrays/contains_duplicate.py
@@ -0,0 +1,7 @@
+# LeetCode 217: Contains Duplicate
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
