8f11aa47aa3f0ade0d6bc7efc40c69df6ebefa4d
Dinesh B <bdinesh170601@gmail.com>
2026-01-10 20:47:28 +0530

    LeetCode 49: Group Anagrams
    Implement a solution to group anagrams from a list of strings.

---
+++ strings/group_anagrams.py
@@ -0,0 +1,16 @@
+# LeetCode 49: Group Anagrams
+# Time Complexity: O(n * k log k)
+# Space Complexity: O(n)
+
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+
+        for word in strs:
+            key = ''.join(sorted(word))   # anagram signature
+            groups[key].append(word)
+
+        return list(groups.values())
